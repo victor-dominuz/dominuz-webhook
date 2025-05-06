aa35a163c1fb2ccbf15ba7b2a1f7647e431a67f0
Victo- Dominuz <victorparis1213@gmail.com>
2025-05-05 21:53:30 -0300

    Usando variáveis de ambiente no webhook

--- API/webhook-stripe.js
+++ API/webhook-stripe.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const stripe = require("stripe")("SUA_CHAVE_SECRETA");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(bodyParser.raw({ type: "application/json" }));
+app.use(bodyParser.raw({ type: 'application/json' }));
 
-app.post("/webhook-stripe", (req, res) => {
-  const sig = req.headers["stripe-signature"];
-  const endpointSecret = "SUA_CHAVE_DO_ENDPOINT";
+app.post('/webhook-stripe', (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 
   let event;
 
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
   } catch (err) {
-    console.log("Webhook signature verification failed:", err.message);
+    console.log("Webhook signature verification failed.", err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
